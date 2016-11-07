bacf004d473a95912d91d6a0a5b90af0c85c808a
Roslyak-Dmitriy <formulaone.forever@gmail.com>
2016-11-07 19:23:42 +0200

    Improved UI
    Added: guest filter; sort by price

--- main_ctrl.js
+++ main_ctrl.js
@@ -3,7 +3,13 @@
  */
 HotelApp.controller('MainCtrl', function ($scope,MainService) {
     $scope.test = 'Hello world';
+    $scope.selected2=['от дешевых к дорогим','от дорогих к дешевым'];
+    $scope.guests = [1,2,3,4];
     $scope.items = [{id:0,price:0,guests:0,type:'',options_id:0,image_src:''}];
+    $scope.orderByprice = function () {
+        if($scope.selected2=='от дешевых к дорогим')return 'price'
+        else return '-price'
+    };
     $scope.show_all = function () {
         MainService.get_all(function (items) {
             $scope.items = items;
